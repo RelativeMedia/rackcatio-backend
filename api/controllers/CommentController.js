24795f2fc52ac95dce8272cf47ed5e42d4a07aed
Mike DeVita <mike@devita.co>
2016-12-14 18:49:55 -0700

    commit

---
+++ api/controllers/CommentController.js
@@ -0,0 +1,9 @@
+/**
+ * Created by mhdevita on 12/14/2016.
+ */
+/**
+ * CommentController
+ * @description :: Server-side logic for manage comments
+ */
+
+module.exports = {};
--- api/services/MailerService.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-const mailer = require('sails-service-mailer');
-const _      = require('lodash');
-const config = _.merge(require('../../config/services/mailer'), require('../../config/local'));
-
-module.exports = mailer('SendGrid', config.services.mailer);
