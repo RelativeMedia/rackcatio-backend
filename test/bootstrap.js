376ecd2dde628016c4604b33de06341d1172e7d4
Mike Devita <mike@relative.media>
2016-12-22 14:07:49 -0700

    Set the baseUrl from sails.config.appUrl
    appUrl is defined in config/env/<env>.js

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -1,9 +1,10 @@
-const assert = require('chai').assert;
 const Sails = require('sails');
 const Barrels = require('barrels');
 const barrels = new Barrels();
-
 const config = require('../config/env/test');
+
+axios = require('axios');
+axios.defaults.baseURL = config.appUrl;
 
 let sails;
 fixtures = barrels.data;
